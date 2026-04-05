8bd764fc20f031555bd403ba22064dc62f906f87
shalalla <whgkdwnd@naver.com>
2026-04-06 00:20:48 +0900

    feat/login

--- backend/src/main/java/com/myfave/api/domain/auth/service/AuthService.java
+++ backend/src/main/java/com/myfave/api/domain/auth/service/AuthService.java
@@ -1,16 +1,23 @@
 package com.myfave.api.domain.auth.service;
 
+import com.myfave.api.domain.auth.dto.request.LoginRequest;
 import com.myfave.api.domain.auth.dto.request.SignUpRequest;
+import com.myfave.api.domain.auth.dto.response.LoginResponse;
 import com.myfave.api.domain.auth.dto.response.SignUpResponse;
 import com.myfave.api.domain.user.entity.User;
 import com.myfave.api.domain.user.repository.UserRepository;
 import com.myfave.api.global.error.CustomException;
 import com.myfave.api.global.error.ErrorCode;
+import com.myfave.api.global.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +27,11 @@ public class AuthService {
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Value("${jwt.refresh-token-expiry}")
+    private long refreshTokenExpiry;
 
     @Transactional
     public SignUpResponse signUp(SignUpRequest request) {
@@ -42,5 +54,26 @@ public class AuthService {
                 .build();
 
         return SignUpResponse.from(userRepository.save(user));
+    }
+
+    public LoginResponse login(LoginRequest request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new CustomException(ErrorCode.AUTH_INVALID_CREDENTIALS));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new CustomException(ErrorCode.AUTH_INVALID_CREDENTIALS);
+        }
+        // 토큰 발급
+        String accessToken = jwtTokenProvider.createAccessToken(user.getUserId());
+        String refreshToken = jwtTokenProvider.createRefreshToken(user.getUserId());
+
+        redisTemplate.opsForValue().set( // radis를 key-value 형태로 사용하겠다.
+                "refresh:" + user.getUserId(), // key
+                refreshToken, // value
+                refreshTokenExpiry, // 유효기간
+                TimeUnit.MILLISECONDS// 단위
+        );
+
+        return LoginResponse.of(accessToken, refreshToken, user);
     }
 }
