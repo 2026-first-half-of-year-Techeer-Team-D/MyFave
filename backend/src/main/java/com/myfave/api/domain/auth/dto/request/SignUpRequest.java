1b40691e35579b8e8804c8f9ecfa950835d7d25f
shalalla <whgkdwnd@naver.com>
2026-04-05 23:47:28 +0900

    feat/ 회원가입기능 구현

--- backend/src/main/java/com/myfave/api/domain/auth/dto/request/SignUpRequest.java
+++ backend/src/main/java/com/myfave/api/domain/auth/dto/request/SignUpRequest.java
@@ -1,13 +1,32 @@
 package com.myfave.api.domain.auth.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
-
+// 회원가입에 필요한 정보 정의
+// 여기서 오류가 발생할 경우 ex) 이메일이 너무 짧음 Spring의 @Valid 어노테이션이 에러 발생
 @Getter
 public class SignUpRequest {
 
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[\\W_]).{8,20}$") //@pattern(regexp = "") <- 정규 표현식과 맞는지 검증하겠다.
     private String password;
+
+    @NotBlank
+    @Size(min = 2, max = 20)
     private String name;
+
+    @NotBlank
+    @Size(min = 2, max = 12)
     private String nickname;
+
+    @NotBlank
+    @Pattern(regexp = "^010-\\d{4}-\\d{4}$")
     private String phone;
 }
--- backend/src/main/java/com/myfave/api/domain/auth/service/AuthService.java
+++ backend/src/main/java/com/myfave/api/domain/auth/service/AuthService.java
@@ -1,6 +1,11 @@
 package com.myfave.api.domain.auth.service;
 
+import com.myfave.api.domain.auth.dto.request.SignUpRequest;
+import com.myfave.api.domain.auth.dto.response.SignUpResponse;
+import com.myfave.api.domain.user.entity.User;
 import com.myfave.api.domain.user.repository.UserRepository;
+import com.myfave.api.global.error.CustomException;
+import com.myfave.api.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,4 +20,27 @@ public class AuthService {
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public SignUpResponse signUp(SignUpRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new CustomException(ErrorCode.USER_DUPLICATE_EMAIL);
+        }
+        if (userRepository.existsByNickname(request.getNickname())) {
+            throw new CustomException(ErrorCode.USER_DUPLICATE_NICKNAME);
+        }
+        if (userRepository.existsByPhone(request.getPhone())) {
+            throw new CustomException(ErrorCode.USER_DUPLICATE_PHONE);
+        }
+
+        User user = User.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .name(request.getName())
+                .nickname(request.getNickname())
+                .phone(request.getPhone())
+                .build();
+
+        return SignUpResponse.from(userRepository.save(user));
+    }
 }
