e9238ac1330d1fc23427703199de787ecb6c5542
0901shc <0901shc@naver.com>
2026-04-21 21:38:32 +0900

    feat: [PaymentMethod.java] CREDITCARD→CARD, TOSS→TOSS_PAY 명칭 변경

--- backend/src/main/java/com/myfave/api/domain/payment/entity/PaymentMethod.java
+++ backend/src/main/java/com/myfave/api/domain/payment/entity/PaymentMethod.java
@@ -1,5 +1,8 @@
 package com.myfave.api.domain.payment.entity;
 
 public enum PaymentMethod {
-    CREDITCARD, TOSS, KAKAO_PAY, NAVER_PAY
+    CARD,       // 신용/체크카드
+    TOSS_PAY,   // 토스페이
+    KAKAO_PAY,
+    NAVER_PAY
 }
