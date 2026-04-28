0fb580fdd687fb8757e29f25a441f56c79bebc50
0901shc <0901shc@naver.com>
2026-04-28 17:50:24 +0900

    refactor: auth/hooks.ts useLogin onSuccess에 스토어 연결, useUser/useIsAuthenticated/useLogout 훅 추가

--- frontend/src/features/auth/hooks.ts
+++ frontend/src/features/auth/hooks.ts
@@ -1,13 +1,30 @@
 import { useMutation } from '@tanstack/react-query'
 
 import { authApi } from './api'
+import { useAuthStore } from './store'
 
 export function useLogin() {
+  const login = useAuthStore((s) => s.login)
   return useMutation({
     mutationFn: authApi.login,
     onSuccess: (data) => {
-      localStorage.setItem('accessToken', data.accessToken)
-      localStorage.setItem('refreshToken', data.refreshToken)
+      login({
+        user: { id: data.userId, email: '', nickname: data.nickname },
+        accessToken: data.accessToken,
+        refreshToken: data.refreshToken,
+      })
     },
   })
+}
+
+export function useUser() {
+  return useAuthStore((s) => s.user)
+}
+
+export function useIsAuthenticated() {
+  return useAuthStore((s) => s.accessToken !== null)
+}
+
+export function useLogout() {
+  return useAuthStore((s) => s.logout)
 }
