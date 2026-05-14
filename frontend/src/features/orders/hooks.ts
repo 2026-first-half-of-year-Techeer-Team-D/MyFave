20abfb4eff13aac8399d1f8ee18bb3015bcf7860
0901shc <0901shc@naver.com>
2026-05-14 12:48:10 +0900

    feat: [orders/hooks.ts] 주문 생성 mutation 훅 추가

--- frontend/src/features/orders/hooks.ts
+++ frontend/src/features/orders/hooks.ts
@@ -1,4 +1,6 @@
+import { useMutation } from '@tanstack/react-query'
 import { useOrderStore } from './store'
+import { ordersApi } from './api'
 
 export function useOrders() {
   return useOrderStore((s) => s.orders)
@@ -6,4 +8,8 @@ export function useOrders() {
 
 export function useOrder(id: string | undefined) {
   return useOrderStore((s) => (id ? s.orders.find((o) => o.id === id) : undefined))
+}
+
+export function useCreateOrder() {
+  return useMutation({ mutationFn: ordersApi.create })
 }
